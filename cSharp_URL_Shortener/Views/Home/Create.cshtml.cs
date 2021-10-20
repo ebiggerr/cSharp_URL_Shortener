f615150c8fa45d21e49268694198fc4e43111bff
Alexious Yong <46164655+ebiggerr@users.noreply.github.com>
2021-10-20 20:54:11 +0800

    Misc
    Signed-off-by: Alexious Yong <46164655+ebiggerr@users.noreply.github.com>

---
+++ cSharp_URL_Shortener/Views/Home/Create.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cSharp_URL_Shortener.Views.Home
+{
+    public class Create : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
