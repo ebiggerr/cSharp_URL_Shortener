f0219d403e2f34d6fcadf6623a3a05616fa60e73
Alexious Yong <46164655+ebiggerr@users.noreply.github.com>
2021-10-20 20:49:32 +0800

    Updated the DbContext
    Signed-off-by: Alexious Yong <46164655+ebiggerr@users.noreply.github.com>

--- cSharp_URL_Shortener/EF/cSharp_URL_ShortenerDbContext.cs
+++ cSharp_URL_Shortener/EF/cSharp_URL_ShortenerDbContext.cs
@@ -1,9 +1,11 @@
-﻿using cSharp_URL_Shortener.Models.Redirect;
+﻿using System;
+using cSharp_URL_Shortener.Models;
+using cSharp_URL_Shortener.Models.Redirect;
 using Microsoft.EntityFrameworkCore;
 
 namespace cSharp_URL_Shortener.EF
 {
-    public class cSharp_URL_ShortenerDbContext: DbContext
+    public class cSharp_URL_ShortenerDbContext: DbContext, IcSharp_URL_ShortenerDbContext
     {
         
         public cSharp_URL_ShortenerDbContext(DbContextOptions<cSharp_URL_ShortenerDbContext> options)
@@ -13,13 +15,19 @@ namespace cSharp_URL_Shortener.EF
         }
         
         public DbSet<Redirect> Redirects { get; set; }
-        
+        public DbSet<Statistics> Statistics { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Indexing
             modelBuilder.Entity<Redirect>()
                 .HasIndex(x => x.ShortenLink)
                 .IsUnique();
+            
+            /*modelBuilder.Entity<Redirect>()
+                .HasRequired<Statistics>(x => x.Redirect)
+                .WithMany(y => y.Redirects)
+                .HasForeignKey<Guid>(z => z.RedirectId);*/     
         }
     }
 }
