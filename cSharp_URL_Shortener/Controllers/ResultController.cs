88a1c5af60ffb723b0f6b33e146c45b8d03aef70
Alexious Yong <46164655+ebiggerr@users.noreply.github.com>
2021-10-24 15:49:51 +0800

    Adding Result controller that will show the shortener result to user
    Signed-off-by: Alexious Yong <46164655+ebiggerr@users.noreply.github.com>

---
+++ cSharp_URL_Shortener/Controllers/ResultController.cs
@@ -0,0 +1,16 @@
+﻿using cSharp_URL_Shortener.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cSharp_URL_Shortener.Controllers
+{
+    public class ResultController: Controller
+    {
+        [Route("/Result")]
+        public IActionResult Index()
+        {
+            string input = "/Home/Create";
+            
+            return View("Result", new Result(input));
+        }
+    }
+}
